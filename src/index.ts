43c3f91f53f426f731450c9026061cbcdf3f0d55
Tyler Russell <33195081+tyzoo@users.noreply.github.com>
2022-08-17 13:27:41 -0400

    on release

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
 export * from './components';
 console.log(`[✨tweb3-components]`);
-
-// "publishConfig": {
-//     "@tyzoo:registry": "https://npm.pkg.github.com"
-// },
-// @tyzoo:registry=https://npm.pkg.github.com
